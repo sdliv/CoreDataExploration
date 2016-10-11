819943951406cbff37e4f994ef379ff024d85ca0
pleiromatics <pleiromatics@Seans-MacBook-Pro-2.local>
2016-10-10 23:29:18 -0400

    Initial work flow.  For CoreData.  More core data Swift 3.0 exploration.

--- CoreDataPlayAround/ViewController.swift
+++ CoreDataPlayAround/ViewController.swift
@@ -7,19 +7,96 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    var people = [NSManagedObject]()
+    
+    @IBOutlet weak var hitListTableView: UITableView!
+    
+    @IBAction func addName(_ sender: AnyObject) {
+        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { (action) in
+            
+            let textField = alert.textFields!.first
+            self.saveName(name: textField!.text!)
+            self.hitListTableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        
+        alert.addTextField { (textfield) in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "\"The List\""
+        hitListTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Person")
+        
+        do {
+            let results = try managedContext.fetch(fetchRequest)
+            people = results as! [NSManagedObject]
+        } catch let error as NSError {
+            print("could not fetch: \(error.description)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        let person = people[indexPath.row]
+        
+        cell?.textLabel?.text = person.value(forKey: "name") as? String
+        return cell!
+    }
 
+    func saveName(name: String) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)
+        
+        let person = NSManagedObject(entity: entity!, insertInto: managedContext)
+        
+        person.setValue(name, forKey: "name")
+        
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            print("Oops, there was an error: \(error.description)")
+            abort()
+        }
+    }
 
 }
 
